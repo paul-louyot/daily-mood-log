2240191bb3ef4205a8d1eadf2cf78e740b365723
paul <pa.louyot@gmail.com>
2020-10-14 21:49:05 +0200

    Use scss

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // This is the main.js file. Import global CSS and scripts here.
 import BootstrapVue from 'bootstrap-vue'
 
-import '~/assets/main.css'
+import '~/assets/main.scss'
 
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 import DefaultLayout from '~/layouts/Default.vue'
