bf256ad017b21abf70ca7d29f311e64fdd9f5ed9
paul <pa.louyot@gmail.com>
2020-11-02 22:30:42 +0100

    .

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -779,18 +779,6 @@ export default {
     deleteBlame(index){
       this.blameList.splice(index, 1);
     },
-    // download(filename, text){
-    //   var element = document.createElement('a');
-    //   element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    //   element.setAttribute('download', filename);
-
-    //   element.style.display = 'none';
-    //   document.body.appendChild(element);
-
-    //   element.click();
-
-    //   document.body.removeChild(element);
-    // },
     downloadPDF(){
       var today = new Date();
       var date = [today.getFullYear(), (today.getMonth() + 1), today.getDate()].join('-');
