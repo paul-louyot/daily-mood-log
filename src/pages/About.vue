947b4cae89f26ed58b34bfd9f8062b3518e387ac
paul <pa.louyot@gmail.com>
2020-11-21 19:54:29 +0100

    Update

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,17 +2,23 @@
   <Layout>
     <b-container>
       <div class="p-4 mb-4 bg-light rounded-lg shadow">
-        <strong>
-          Ce site enregistre-t-il les données de ses utilisateurs ?
-        </strong>
-        <div>
-          Non. Les données ne sont pas non plus enregistrées dans votre ordinateur.
-          La seule manière d'enregistrer votre activité sur ce site est de génerer un compte-rendu PDF.
+        <p>
+          <strong>
+            Mon activité sur ce site est-celle enregistrée ?
+          </strong>
+        </p>
+        <p>
+          Non.
+        </p>
+        <p>
+           Les données ne sont pas non plus enregistrées dans votre ordinateur, sauf si vous téléchargez le PDF de votre compte-rendu.
+        </p>
+        <p>
           Le code source de ce site internet est public et consultable à cette adresse :
           <a href="https://github.com/paul-louyot/daily-mood-log" target="_blank">
             github.com/paul-louyot/daily-mood-log
           </a>
-        </div>
+        </p>
       </div>
     </b-container>
   </Layout>
